e521d863a28ba8554f3a6b90619715b3c6f9230f
tsa2669978 <2669978@collegelacite.ca>
2022-05-02 11:33:42 -0400

    Testing deploy

--- model/sendEmail.js
+++ model/sendEmail.js
@@ -1,27 +1,8 @@
-import { emailjs } from "emailjs-com";
 
 
 export  const sendEmail = (name,email,message) =>{
 
 
-    var data = {
-        service_id: 'service_ldvuy0i',
-        template_id: 'template_hzfsl3r',
-        user_id: 'DSa-nJXJkMyncUACi',
-        template_params: {
-            'name': name,
-            'email':email,
-            'message':message
-        }
-    };
-     
-    emailjs.send('service_ldvuy0i', 'template_hzfsl3r', data)
-        .then(function(response) {
-           console.log('SUCCESS!', response.status, response.text);
-        }, function(error) {
-           console.log('FAILED...', error);
-        });
-   
     console.log('Name: '+ name);
     console.log('email: '+ email);
     console.log('message: '+ message);
